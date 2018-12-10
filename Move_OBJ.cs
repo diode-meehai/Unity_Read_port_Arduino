1a26ee46fe53a50f14cac9611da825eaad85054f
Diode <ponza.zaza11@gmail.com>
2018-12-10 11:32:54 +0700

    Add files via upload

---
+++ Move_OBJ.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO.Ports;
+
+public class Move_OBJ : MonoBehaviour {
+
+    int Check_Value = 0; // Variable check volume (VR) 
+    SerialPort Arduino_Port = new SerialPort("COM4", 9600); //Variable = port number,baudrate
+
+    // Use this for initialization
+    void Start()
+    {
+        Arduino_Port.Open(); // Open port
+        Arduino_Port.ReadTimeout = 1; // Time Rate Read port
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Arduino_Port.IsOpen) // Check port TURE
+        {
+            try
+            {
+                string Read_P = Arduino_Port.ReadLine(); // Read port
+                //print(Read_P); // print console read port
+                int X = int.Parse(Read_P); // strint to int
+
+
+                //===================Limit Move scale=====X=Y=Z========================//
+                Vector3 _limit = transform.position;
+                _limit.x = Mathf.Clamp(_limit.x, -3.0f, 3);
+                // _limit.y = Mathf.Clamp(_limit.y, -2.7f, 2.3f);
+                // _limit.z = Mathf.Clamp(_limit.y, 0, 0.1f);
+                transform.position = _limit;
+                //=================================================================//
+
+                //=================Check changes===VR============================//
+                if (X > 511 && X > Check_Value)
+                {
+                    Check_Value = X;
+                    int Move_R = 0;
+                    Move_R += 1;
+                    transform.Translate(1 * Move_R, 0, 0 * Time.deltaTime);  // Move object right
+                }
+                else if (X < 511 && X < Check_Value)
+                {
+                    Check_Value = X;
+                    int Move_R = 0;
+                    Move_R -= 1;
+                    transform.Translate(1 * Move_R, 0, 0 * Time.deltaTime); // Move object left
+                }
+                //=============================================================//
+
+            }
+            catch (System.Exception) { }
+        }
+
+    }
+}
---
+++ Random_move.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_move : MonoBehaviour {
+
+    public int _speed = -10; // Variable Start speed
+    public AudioClip _HitSFX; // Variable Audio
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(0, 0, _speed * Time.deltaTime); // Move object axis Z
+
+        if (transform.position.z < -8) // Stop object axis Z = -8
+        {
+            _newPos(); // Run function
+        }
+
+    }
+
+    //===================Random Start object axis Z =================//
+    public void _newPos()
+    {
+        float _random = Random.Range(-10.0f, 10.0f); //-5, 6
+        transform.position = new Vector3(_random, -0.8f, 6);
+        _speed = 0;
+
+        Invoke("_newSpeed", 0); // Time Start object >> function _newSpeed
+
+    }
+    //=========================================================//
+
+    //===================Random speed object====================//
+    public void _newSpeed()
+    {
+        _speed = Random.Range(-20, -10);
+    }
+    //=========================================================//
+
+    //==============Collision detection=======================//
+    private void OnCollisionEnter(Collision _Chonh)
+    {
+        if (_Chonh.collider.name == "Cylinder_P") // object name Cylinder_P
+        {
+            GetComponent<AudioSource>().PlayOneShot(_HitSFX); // Play sound
+            //_parPoint_2 = (Transform)Instantiate(_parFX, transform.position, transform.rotation);
+            //_speed = -1;
+            //Destroy(_Chonh.gameObject); //ทำลายตัวอื่น
+            //Destroy(gameObject); //ทำลายตัวเอง
+            print("OK!!"); // print console
+            _newPos(); // start function, restart object
+        }
+    }
+    //=========================================================//
+}
