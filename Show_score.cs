ab71c9b675134bfbd0d79f7d61bde8e32647e092
Diode <ponza.zaza11@gmail.com>
2018-12-20 12:03:53 +0700

    Add files via upload

---
+++ Show_score.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Show_score : MonoBehaviour {
+
+    static public int _score;
+
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponent<TextMesh>().text = "Score: " + _score;  //Show Text Score
+
+        if (Time_out._Time_in == 0)
+        {
+            Time.timeScale = 0; //Pause
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _score = 0;
+            Time.timeScale = 1; //Play
+            Time_out._Time_in = 5; //Reset time
+        }
+
+    }
+}
---
+++ Time_out.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time_out : MonoBehaviour {
+
+    public int _timeOut;
+    static public int _Time_in;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _Time_in = _timeOut;
+
+        //----------------Count-Time----------------
+        InvokeRepeating("_countDown", 1, 1);
+        //------------------------------------------
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMesh>().text = "Time : " + _Time_in; //Show time
+    }
+
+    public void _countDown()
+    {
+        _Time_in -= 1; // time -1
+    }
+}
